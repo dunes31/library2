ffa2069afa5f1078bc6691d97d59c0c9d87c0016
dunes31 <dunesfactory31@gmail.com>
2023-02-27 16:21:50 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2023 Online Library Management System |<a href=# target="_blank"  > Designed by : Ritik Batra</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
